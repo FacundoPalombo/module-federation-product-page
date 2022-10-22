05764ce620ba4c77ee53c780208ea5349d501b69
Facundo Palombo facundo.palombo@gmail.com <facundo.palombo@gmail.com>
2022-10-22 15:36:03 -0300

    feature: initial scaffolding and features from marketing

---
+++ marketing/src/bootstrap.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+// Mount function to start up the app
+const mount = (el) => {
+  ReactDOM.render(<App />, el);
+};
+// If we are in development and in isolation call mount inmediately on devRoot.
+if (process.env.NODE_ENV === "development") {
+  const devRoot = document.getElementById("_marketing-dev-root");
+  if (devRoot) mount(devRoot);
+}
+
+// If we are running trough our container we should export the mount function
